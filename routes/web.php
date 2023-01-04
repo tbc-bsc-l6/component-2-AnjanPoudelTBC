adbbace77080203795f14faa07da2f4f4eb9669c
Anjan_poudel <panjan19@tbc.edu.np>
2023-01-04 22:07:43 +0545

    login,register, forget password route and customisation

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,12 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ProductsController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/product/{id}', [ProductsController::class, 'show'])->where(['id' => '[0-9]{1,5}']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/store-form', [ProductsController::class, 'store']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::patch('/product/{id}', [ProductsController::class, 'update']);
-
-Route::delete('/product/{id}', [ProductsController::class, 'destroy']);
+require __DIR__ . '/auth.php';
