9546733269d2bbb07812435feba546dc604a4cf0
Anjan-Poudel-1 <panjan19@tbc.edu.np>
2023-01-12 22:43:35 +0545

    update user password

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password;
 
@@ -15,15 +17,20 @@ class PasswordController extends Controller
      */
     public function update(Request $request): RedirectResponse
     {
+
         $validated = $request->validateWithBag('updatePassword', [
             'current_password' => ['required', 'current_password'],
             'password' => ['required', Password::defaults(), 'confirmed'],
         ]);
 
-        $request->user()->update([
-            'password' => Hash::make($validated['password']),
-        ]);
 
-        return back()->with('status', 'password-updated');
+
+        #Update the new Password
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($validated['password'])
+        ]);
+        Auth::logout();
+
+        return redirect('')->with("success", "Password changed successfully!");
     }
 }
--- resources/views/profile/my-profile.blade.php
+++ resources/views/profile/my-profile.blade.php
@@ -69,29 +69,32 @@
                     <div class=" mb-4 text-xl font-semibold">
                         Reset Your Password
                     </div>
-                    <form action="" class="form-blank password-reset-form">
+                    <form method="POST" action="{{route('password.update')}}" class="form-blank password-reset-form">
+                        @csrf
+                        @method('PUT')
                         <div class="mt-3">
-                            <x-input-label for="product_name" :value="__('User Name')" />
-                            <x-text-input placeholder="Enter the product name" id="product_name" type="text"
-                                name='product_name' value="{{old('product_name')}}" required />
-                            <x-input-error :messages="$errors->get('product_name')" class="mt-2" />
+                            <x-input-label for="current_password" :value="__('Current Password')" />
+                            <x-text-input placeholder="Enter your current password" id="current_password"
+                                type="password" name='current_password' required />
+                            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
                         </div>
                         <div class="mt-3">
-                            <x-input-label for="product_name" :value="__('Email Address')" />
-                            <x-text-input placeholder="Enter the product name" id="product_name" type="text"
-                                name='product_name' value="{{old('product_name')}}" :disabled=true />
-                            <x-input-error :messages="$errors->get('product_name')" class="mt-2" />
+                            <x-input-label for="password" :value="__('New Password')" />
+                            <x-text-input placeholder="Enter the new password" id="password" type="password"
+                                name='password' value="{{old('password')}}" required />
+                            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
                         </div>
 
                         <div class="mt-3">
-                            <x-input-label for="product_name" :value="__('Email Address')" />
-                            <x-text-input placeholder="Enter the product name" id="product_name" type="text"
-                                name='product_name' value="{{old('product_name')}}" :disabled=true />
-                            <x-input-error :messages="$errors->get('product_name')" class="mt-2" />
+                            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                            <x-text-input placeholder="Confirm your password" id="password_confirmation" type="password"
+                                name='password_confirmation' value="{{old('password_confirmation')}}" required />
+                            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')"
+                                class="mt-2" />
                         </div>
 
 
-                        <button class="btn btn-danger mt-4">
+                        <button type="submit" class="btn btn-danger mt-4">
                             Reset Password
                         </button>
                     </form>
